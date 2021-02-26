1a30f001ef096a7b8e1d30d9f63da53a027fad53
Reginaldo <reginaldo_00@hotmail.com>
2021-02-26 10:22:19 -0300

    alterado home

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Reginaldo',
-      sobrenome: 'Santa',
-      email: 'reginaldo_00@hotmail.com',
-      idade: 10,
-      peso: 300,
-      altura: 2.5,
-    });
-    res.status(200).json(novoAluno);
+    res.json('Index');
   }
 }
 
